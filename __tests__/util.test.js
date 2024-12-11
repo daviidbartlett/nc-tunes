da9007aecf5f76063e454461bf6a7bfec05ed4b8
David Bartlett <david@northcoders.com>
2024-12-11 14:15:00 +0000

    add util test job

---
+++ __tests__/util.test.js
@@ -0,0 +1,17 @@
+describe("test", () => {
+  test("should first", () => {
+    expect(2).toBe(2);
+  });
+  test("should first", () => {
+    expect(2).toBe(2);
+  });
+  test("should first", () => {
+    expect(2).toBe(2);
+  });
+  test("should first", () => {
+    expect(2).toBe(2);
+  });
+  test("should first", () => {
+    expect(2).toBe(2);
+  });
+});
